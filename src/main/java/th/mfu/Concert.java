40279ac2c4336d5e3656ea443716a5cdc2af462f
Kongphop saenphai <6531503008@lamduan.mfu.ac.th>
2023-09-11 23:41:02 +0700

    Commit eiei

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -3,24 +3,61 @@ package th.mfu;
 import java.util.Date;
 
 public class Concert {
-
-    //TODO: add attributes
+    private int id;
+    private String title;
+    private String description;
+    private String performar;
+    private Date date;
 
     public Concert() {
     }
+
     public Concert(String title, String description) {
-        //TODO: set attributes
+        // TODO: set attributes
+        this.title = title;
+        this.description = description;
     }
- 
+
     public String getTitle() {
-        //TODO: return title
-        return "";
+        // TODO: return title
+        return title;
     }
+
     public String getDescription() {
-        //TODO: return description
-        return "";
+        // TODO: return description
+        return description;
     }
 
-    //TODO: add getters and setters
+    // TODO: add getters and setters
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPerformer() {
+        return performar;
+    }
+
+    public void setPerformer(String performer) {
+        this.performar = performer;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/th/mfu/ConcertController.java
+++ src/main/java/th/mfu/ConcertController.java
@@ -19,45 +19,51 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ConcertController {
-    // TODO: create hashmap of concerts for storing data
+    private int nextId = 1;
+    private HashMap<Integer,Concert> concerts = new HashMap<Integer,Concert>(); // In-memory storage for simplicity
 
-    //TODO: add initbinder to convert date
+    
+    @InitBinder
+    public final void initBinderUsuariosFormValidator(final WebDataBinder binder, final Locale locale) {
+        final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", locale);
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+    }
+
 
     @GetMapping("/concerts")
     public String listConcerts(Model model) {
-        // TODO: add concerts to model
-        // TODO: return a template to list concerts
-        return "";
+        model.addAttribute("concerts", concerts.values());
+        return "list-concert";
     }
 
     @GetMapping("/add-concert")
-    public String addAConcertForm(Model model) {
-        // TODO: pass blank concert to a form
-        // TODO: return a template for concert form
-        return "";
+    public String showAddConcertForm(Model model) {
+        // pass blank concert to a form
+        model.addAttribute("concert", new Concert());
+        return "add-concert-form";
     }
 
     @PostMapping("/concerts")
     public String saveConcert(@ModelAttribute Concert concert) {
-        // TODO: add concert to list of concerts
-        // TODO: increment nextId
-        // TODO: redirect to list concerts
-        return "";
+        // In a real application, you would save the concert to a database or other storage
+        concert.setId(nextId);
+        concerts.put(nextId, concert);
+        nextId++;
+        return "redirect:/concerts";
     }
 
     @GetMapping("/delete-concert/{id}")
-    public String deleteConcert(@PathVariable int id) {
-        // TODO: remove concert from list of concerts
-        // TODO: redirect to list concerts
-        return "";
+    public String removeConcert(@PathVariable int id) {
+        // pass blank concert to a form
+        concerts.remove(id);
+        return "redirect:/concerts";
     }
 
-    
     @GetMapping("/delete-concert")
-    public String removeAllConcerts() {
-        //TODO: clear all employees and reset id
-        // TODO: redirect to list concerts
-        return "";
+    public String removeAllConcert() {
+        // pass blank concert to a form
+        concerts.clear();
+        nextId = 1;
+        return "redirect:/concerts";
     }
-
 }
